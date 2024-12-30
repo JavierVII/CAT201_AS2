721f60267ef08d78f56ae47017129452e5c3808d
JavierVII <cw041125@gmail.com>
2024-12-30 21:40:22 +0800

    导航栏√

--- visitPenang/src/components/HeroSection.js
+++ visitPenang/src/components/HeroSection.js
@@ -5,12 +5,10 @@ const HeroSection = () => {
     return (
         <div className="hero-section">
             {/* 背景视频 */}
-            <video autoPlay loop muted className="background-video">
+            <video autoPlay loop className="background-video">
                 <source src="/videos/penang_video.mp4" type="video/mp4" />
                 您的浏览器不支持视频播放。
             </video>
-
-
         </div>
     );
 };
